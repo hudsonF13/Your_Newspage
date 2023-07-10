cad9b6898460c3e5d744c2a99c537330aff02d17
Hudson Ferreria <hudsonferre13@gmail.com>
2023-07-10 12:22:17 -0300

    first commit

---
+++ news-homepage-main/index.js
@@ -0,0 +1,7 @@
+const newsButton = document.getElementById("newsButton");
+
+function readMore() {
+    window.open("https://codepen.io/hudsonF13")
+}
+
+newsButton.addEventListener("click", readMore);
