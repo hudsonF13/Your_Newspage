b7a72ace4c21000e6e3d14081aa8a7f18922e103
Hudson Ferreria <hudsonferre13@gmail.com>
2023-07-11 09:47:04 -0300

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const newsButton = document.getElementById("newsButton");
+const showNav = document.getElementById("showMeLinks");
 
 function readMore() {
     window.open("https://codepen.io/hudsonF13")
 }
 
+function showUpNavigtion() {
+    
+}
+
 newsButton.addEventListener("click", readMore);
+showNav.addEventeListenner("click", showUpNavigtion)
